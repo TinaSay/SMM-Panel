4834a0558843053ca4d4728aec0951d3663c849d
sayapina-tina <valechka-sayapina@mail.ru>
2019-03-06 18:41:49 +0500

    odnoklassniki Auth, Tasks CRUD

---
+++ app/Modules/Bosslike/Controllers/OkController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Modules\Bosslike\Controllers;
+
+use App\Modules\Bosslike\Models\SocialUser;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+/**
+ * Class OkController
+ * @package App\Modules\Bosslike\Controllers
+ */
+class OkController extends Controller
+{
+    public function delete($id)
+    {
+        $okUser = SocialUser::findOrFail($id);
+        $okUser->delete();
+        return redirect()->route('profile');
+    }
+
+}
